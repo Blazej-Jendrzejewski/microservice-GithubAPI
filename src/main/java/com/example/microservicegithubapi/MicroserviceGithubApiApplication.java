73a2f31a1a3444d1714247e37f321416920a37f0
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-08-28 17:36:43 +0200

    Initial commit

---
+++ src/main/java/com/example/microservicegithubapi/MicroserviceGithubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicegithubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGithubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGithubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicegithubapi/MicroserviceGithubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicegithubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGithubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
