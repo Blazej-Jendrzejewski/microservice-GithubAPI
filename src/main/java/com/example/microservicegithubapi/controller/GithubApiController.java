aa881def9e5adc43c059b12cad7d94d69820bfae
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-08-29 07:22:50 +0200

    Add controller for getting public repos of a given user

--- src/main/java/com/example/microservicegithubapi/controller/GithubApiController.java
+++ src/main/java/com/example/microservicegithubapi/controller/GithubApiController.java
@@ -1,4 +1,26 @@
 package com.example.microservicegithubapi.controller;
 
+import com.example.microservicegithubapi.model.Repo;
+import com.example.microservicegithubapi.service.GithubApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
 public class GithubApiController {
+
+    @Autowired
+    GithubApiService githubApiService;
+
+    @GetMapping("/api/github/repos/{username}")
+    public List<Repo> getPublicReposForUser(@PathVariable String username){
+        return githubApiService.getUserPublicRepos(username);
+    }
+
+
+
+
 }
