e5c33b9b38dbd32cac8ac29d18993133ffdfdf03
Blazej-Jendrzejewski <blazej.jendrzejewski@gmail.com>
2023-08-29 06:32:00 +0200

    Added rest template bean and @autowired in service

--- src/main/java/com/example/microservicegithubapi/MicroserviceGithubApiApplication.java
+++ src/main/java/com/example/microservicegithubapi/MicroserviceGithubApiApplication.java
@@ -2,6 +2,8 @@ package com.example.microservicegithubapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroserviceGithubApiApplication {
@@ -10,4 +12,8 @@ public class MicroserviceGithubApiApplication {
         SpringApplication.run(MicroserviceGithubApiApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/example/microservicegithubapi/service/GithubApiService.java
+++ src/main/java/com/example/microservicegithubapi/service/GithubApiService.java
@@ -1,8 +1,20 @@
 package com.example.microservicegithubapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class GithubApiService {
 
+    @Value("${github.api.url}")
+    private String githubApiUrl;
 
+    private final RestTemplate restTemplate;
 
-
+    @Autowired
+    public GithubApiService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 }
